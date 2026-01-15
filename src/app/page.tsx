df18c4bacb6e20ee8f5992a672ca3c7040eea871
Rebellious <ratigamer19@gmail.com>
2026-01-15 12:13:34 +0000

    video starter position should be right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           </header>
 
           <div className="mt-20 flex flex-col gap-[112px]">
-            <div className="relative flex justify-start items-start w-full h-[40vh]">
+            <div className="relative flex justify-end items-start w-full h-[40vh]">
               <div className="flex flex-col items-start gap-1 pt-2">
                 <span className="font-bold text-base leading-tight">
                   Scroll Me
