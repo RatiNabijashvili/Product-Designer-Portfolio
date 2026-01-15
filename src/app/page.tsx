b130e45a37692f1ba92139ed1349e2158ad10454
Rebellious <ratigamer19@gmail.com>
2026-01-15 11:39:36 +0000

    Look you have container where Icon and Scroll Me text is wrapped. It is

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,11 +22,11 @@ export default function Home() {
         <div className="mt-20 flex flex-col gap-[112px]">
           {/* Upper Block */}
           <section className="flex justify-between items-start w-full">
-            <div className="flex items-center gap-1 pt-2">
-              <ArrowDown className="size-6" />
+            <div className="flex flex-col items-start gap-1 pt-2">
               <span className="font-bold text-base leading-tight">
                 Scroll Me
               </span>
+              <ArrowDown className="size-6" />
             </div>
             {heroVideoImage ? (
               <div className="w-[568px] h-[320px] bg-muted rounded-xl overflow-hidden shadow-lg">
