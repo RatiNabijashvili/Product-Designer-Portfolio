c6193447165964e7ef56375f72fa1d6aad7cf15f
Rebellious <ratigamer19@gmail.com>
2026-01-16 15:40:42 +0000

    <p class="text-center"> in this phylosophy text is not be centered, it s

--- src/components/shared/philosophy-section.tsx
+++ src/components/shared/philosophy-section.tsx
@@ -3,20 +3,17 @@ export function PhilosophySection() {
   return (
     <section className="min-h-screen flex items-center justify-center px-8">
       <div className="text-[32px] leading-normal max-w-4xl font-body uppercase font-bold">
-        <p className="text-center">
+        <p className="text-left">
           <span className="text-primary">
-            I design with purpose, creating clarity, simplicity and
+            I design with purpose, creating clarity, simplicity and experiences that feel effortless.
           </span>
-        </p>
-        <div className="text-left">
-          <span className="text-primary">experiences that feel effortless.</span>
           <span className="text-[#919191]">
             {' '}
             Every detail matters and every choice has meaning. My work reflects
             how I live: focused, intentional and always thinking several steps
             ahead.
           </span>
-        </div>
+        </p>
       </div>
     </section>
   );
