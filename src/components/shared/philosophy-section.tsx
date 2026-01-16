838d3364b8a034455e00271591bd276a43b4d097
Rebellious <ratigamer19@gmail.com>
2026-01-16 15:45:37 +0000

    Now first span I design with purpose, creating clarity, simplicity and s

--- src/components/shared/philosophy-section.tsx
+++ src/components/shared/philosophy-section.tsx
@@ -2,11 +2,11 @@
 export function PhilosophySection() {
   return (
     <section className="min-h-screen flex items-center justify-center px-8">
-      <div className="text-[32px] leading-normal max-w-7xl font-body uppercase text-left">
-        <p className="font-bold text-primary">
+      <div className="text-[32px] leading-normal max-w-7xl font-body uppercase">
+        <p className="font-bold text-primary text-right">
           I design with purpose, creating clarity, simplicity and
         </p>
-        <p>
+        <p className="text-left">
           <span className="font-bold text-primary">experiences that</span>
           <span className="text-[#919191]">
             {' '}
