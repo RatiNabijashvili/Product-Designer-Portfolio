a8ac47776883cae05a034c7b83c00263982a8e0d
Rebellious <ratigamer19@gmail.com>
2026-01-20 14:52:04 +0000

    Do this - Vertical Centering: I'll vertically center the content of both

--- src/components/shared/work-section.tsx
+++ src/components/shared/work-section.tsx
@@ -24,10 +24,10 @@ export function WorkSection() {
           return (
             <div
               key={project.id}
-              className="grid grid-cols-1 lg:grid-cols-2 gap-12 py-8 border-t border-[#DCDCDC]"
+              className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center py-8 border-t border-[#DCDCDC]"
             >
               {/* Left Column */}
-              <div className="flex flex-col justify-center space-y-12">
+              <div className="flex flex-col justify-center space-y-8">
                 <div className="flex flex-wrap gap-3">
                   {project.tags.map((tag) => (
                     <div
